4a099d531c0aae957335bbd4087ce72bf225e668
YZBRH <140364100+YZBRH@users.noreply.github.com>
2024-07-15 15:21:52 +0800

    Add files via upload

--- encode.py
+++ encode.py
@@ -12,7 +12,7 @@ def crypt_jwxt(msg):
 def crypt_vpn(msg, salt, mode, basedir):
     with open("./jscode/encrypt.js", "r", encoding="utf-8") as f:
         js_code = f.read()
-    ctx = execjs.compile(js_code, cwd=basedir)
+        ctx = execjs.compile(js_code, cwd=basedir)
     if mode == 0:  # 加密模式
         get_msg = ctx.call("encryptPassword", msg, salt)
     else:  # 解密模式
